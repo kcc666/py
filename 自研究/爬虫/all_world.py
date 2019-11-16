7b0f951865fc6f3982221ff289f0db31398be630
kcc666 <463217787@qq.com>
2019-11-16 22:35:54 +0800

    ip地址算法

--- 自研究/爬虫/all_world.py
+++ 自研究/爬虫/all_world.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 # author: kcc time:2019/11/16
 
-import requests,re,random,threading,time
+import requests,re,random,threading,time,os
 
 
 def scrapy_ip():
@@ -26,15 +26,73 @@ def scrapy_ip():
                         print("*" * 40)
                         print(ip)
                         print(title[0])
-        except BaseException as e:
+        except:
             pass
 
 # for i in range(200):
 #     t1 = threading.Thread(target=scrapy_ip)
 #     t1.start()
-for a in range(0,256):
-    for b in range(0, 256):
-        for c in range(0, 256):
-            for d in range(0, 256):
-                print(a,b,c,d,sep=".")
+# count = 0
+# for a in range(0,256):
+#     for b in range(0, 256):
+#         for c in range(0, 256):
+#             for d in range(0, 256):
+#                 count+=1
+#                 print(a,b,c,d,sep=".")
+#                 print(count)
+#                 if count == a==1:
+#                     time.sleep(1000)
+
+#0.0.0.0
+#0
+
+#0.0.1.0
+#256
+
+#0.1.0.0
+#256*256
+
+#1.0.0.0
+#256*256*256
+os.system("cls")
+print("*"*40)
+
+
+def ipv4(n):
+    n-=1
+    if n >= 16777216:
+        ip_1 = int(n/16777216)
+        ip_1s = n%16777216
+        ip_2 = int(ip_1s/65536)
+        ip_2s = ip_1s%65536
+        ip_3 = int(ip_2s/256)
+        ip_3s = ip_2s%256
+        ip_4 = ip_3s
+        print(ip_1,ip_2,ip_3,ip_4,sep=".")
+    elif n<16777216 and n>=65536:
+        ip_1 = 0
+        ip_2 = int(n/65536)
+        ip_2s = n%65536
+        ip_3 = int(ip_2s/256)
+        ip_3s = ip_2s%256
+        ip_4 = ip_3s
+        print(ip_1,ip_2,ip_3,ip_4,sep=".")
+    elif n<65536 and n>=256:
+        ip_1 = 0
+        ip_2 = 0
+        ip_3 = int(n/256)
+        ip_3s = n%256
+        ip_4 = ip_3s
+        print(ip_1,ip_2,ip_3,ip_4,sep=".")
+    else:
+        print("0.0.0.{}".format(n))
+    
+
+
+
+
+ipv4(257)
+print("*"*40)
+
+
 
