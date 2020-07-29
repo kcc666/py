c51f33cf3f7ba0e1e86b3ffa7f926ad5c1e26c3f
kcc666 <463217787@qq.com>
2020-07-29 21:10:40 +0800

    竞技场,试练

--- 神武项目/神武综合脚本.py
+++ 神武项目/神武综合脚本.py
@@ -4,9 +4,7 @@ import 神武竞技场,神武试炼,time,traceback
 while 1:
 
     try:
-
         r = input("神武竞技场请输入1\n神武试炼请输入2\n输入完后按回车开始:\n")
-
         if r == "1":
             print("10秒后开始执行神武竞技场,过程中请勿操作鼠标")
             time.sleep(10)
