377bd123ef3593eb0ada89f028ccbd96d0d78476
kcc666 <463217787@qq.com>
2020-08-02 23:25:39 +0800

    新增宝图任务自动化

--- 神武项目/神武综合脚本.py
+++ 神武项目/神武综合脚本.py
@@ -1,18 +1,22 @@
-import 神武竞技场,神武试炼,time,traceback
+import 神武竞技场,神武试炼,time,traceback,神武宝图
 
 
 while 1:
 
     try:
-        r = input("神武竞技场请输入1\n神武试炼请输入2\n输入完后按回车开始:\n")
+        r = input("神武竞技场请输入1\n神武试炼请输入2\n神武宝图请输入3\n输入完后按回车开始:\n")
         if r == "1":
-            print("10秒后开始执行神武竞技场,过程中请勿操作鼠标")
-            time.sleep(10)
+            print("8秒后开始执行神武竞技场,过程中请勿操作鼠标")
+            time.sleep(8)
             神武竞技场.竞技场()
         elif r == "2":
-            print("10秒后开始执行神武试练,过程中请勿操作鼠标")
-            time.sleep(10)
+            print("8秒后开始执行神武试练,过程中请勿操作鼠标")
+            time.sleep(8)
             神武试炼.英雄试练()
+        elif r == "3":
+            print("8秒后开始执行宝图任务,过程中请勿操作鼠标")
+            time.sleep(8)
+            神武宝图.开始宝图()
         else:
             print("你的输入无效,请重新输入")
     except Exception as e:
